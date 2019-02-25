5e9776ab5da66ed4dd20754fa035b517369ce240
Deni Alfaro <denilorenaalfaro@hotmail.com>
2019-02-25 13:52:10 -0700

    Codigo
    falta que no atraviese el piso

---
+++ Assets/MovimientoCanica.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCanica : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2d.AddForce(Vector2.left * 700, ForceMode2D.Force);
+    }
+}
